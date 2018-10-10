19191fe968e88452bb94b15c7ca325aa04b96ea3
amrithm98 <amrithchallenger@gmail.com>
2018-10-10 12:43:53 +0530

    Cycle 3 Operator Precedence

---
+++ Cycle-3-Parsers/Operator-precedence-parser/stack.h
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+template <typename T>
+class Stack
+{
+    public:
+        vector<T> st;
+
+        Stack()
+        {
+
+        }
+
+        bool empty()
+        {
+            return st.size() == 0;
+        }
+
+        T top()
+        {
+            if(st.empty())
+                return -1;
+            else   
+                return st[st.size()-1];
+        }
+
+        void pop()
+        {
+            if(st.empty())
+            {
+                return;
+            }
+            else
+                st.pop_back();
+        }
+
+        void push(T data)
+        {
+            st.push_back(data);
+        }
+
+        void print()
+        {
+            for(int i = 0; i < st.size(); i++)
+                cout << st[i] << " ";
+            cout << endl;
+        }
+};
--- Cycle-3-Parsers/operator-precedence-parser/operator-precedence.cpp
+++ Cycle-3-Parsers/operator-precedence-parser/operator-precedence.cpp
@@ -1,12 +1,57 @@
 #include <bits/stdc++.h>
+#include "stack.h"
 using namespace std;
 
 vector<vector<int>> precedenceTable(4, vector<int>(4,0));
 map<char,int> opToId;
 
-void operatorPrecedenceParse(string s)
+bool operatorPrecedenceParse(string s)
 {
+    s += '$';
 
+    Stack<char> st;
+    st.push('$');
+
+    int i = 0;
+
+    while(i < s.size())
+    {
+        char b = st.top();
+        char a = s[i];
+        
+        int prec_b = opToId[b];
+        int prec_a = opToId[a];
+
+        cout << "\nState : " << a << " " << b << " " << endl;
+
+        if(b == '$' && a == '$')
+        {
+            return true;
+        }
+
+        st.print();
+
+        if(precedenceTable[prec_a][prec_b] >= 1)
+        {
+            st.push(a);
+            i++;
+        }
+        else
+        {
+            if(!st.empty())
+            {
+                char c = st.top();
+                while(c != '$' && !st.empty() && precedenceTable[opToId[a]][opToId[c]] < 1)
+                {
+                    st.pop();
+                    if(!st.empty())
+                        c = st.top();
+                }
+            }
+        }
+    }
+
+    return false;
 }
 
 void setup()
@@ -20,6 +65,22 @@ void setup()
     precedenceTable[0][1] = 1;
     precedenceTable[0][2] = 1;
     precedenceTable[0][3] = 1;
+
+    precedenceTable[1][0] = 0;
+    precedenceTable[1][1] = 1;
+    precedenceTable[1][2] = 0;
+    precedenceTable[1][3] = 1;
+
+    precedenceTable[2][0] = 0;
+    precedenceTable[2][1] = 1;
+    precedenceTable[2][2] = 1;
+    precedenceTable[2][3] = 1;
+
+    precedenceTable[3][0] = 0;
+    precedenceTable[3][1] = 0;
+    precedenceTable[3][2] = 0;
+    precedenceTable[3][3] = 2;
+    
 }
 
 int main()
@@ -30,6 +91,7 @@ int main()
     string s;
     cin >> s;
 
-    operatorPrecedenceParse(s);
+    cout << operatorPrecedenceParse(s);
+
     return 0;
 }
