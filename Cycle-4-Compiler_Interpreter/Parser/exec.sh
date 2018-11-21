8bead6232e667aa2bb6482d096c41187604f421e
amrithm98 <amrithchallenger@gmail.com>
2018-11-21 18:08:30 +0530

    Working on Custom RDP

--- Cycle-4-Compiler_Interpreter/Parser/exec.sh
+++ Cycle-4-Compiler_Interpreter/Parser/exec.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 rm lex.yy.c
-rm a.out
+rm interp
+rm mc
 rm y.tab.c
 rm y.tab.h
 
 lex comp.l
 yacc -d comp.y
 g++ lex.yy.c y.tab.c -o mc
-./mc < inp.i
+./mc < sample.i
 
 gcc interpreter.c -o interp
 ./interp machine_code
---
+++ Cycle-4-Compiler_Interpreter/Parser/sample.i
@@ -0,0 +1,9 @@
+prog
+    integer a,b,c,d.
+    begin
+        read b;
+        read c;
+        read d;
+        a := 5 + b + c*100;
+        write a;
+    end
